8dd34b2009e2baed2c8000b9b60d2bff6b393d5c
Dmitry Galushko <44999145+Popiron@users.noreply.github.com>
2021-12-02 20:15:02 +0300

    add tetrahedron

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,4 +1,4 @@
-// Рисует зелёный треугольник
+// Рисует кубик в клеточку
 
 #include <gl/glew.h>
 #include <SFML/OpenGL.hpp>
@@ -6,38 +6,92 @@
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
-using namespace std;
 
 
 // Переменные с индентификаторами ID
 // ID шейдерной программы
 GLuint Program;
-// ID атрибута
+// ID атрибута вершин
 GLint Attrib_vertex;
-// ID Vertex Buffer Object
-GLuint VBO;
+// ID атрибута цвета
+GLint Attrib_color;
+// ID юниформ переменной цвета
+GLint Unif_color;
+// ID VBO вершин
+GLuint VBO_position;
+// ID VBO цвета
+GLuint VBO_color;
+
+GLint Unif_translate_x;
+GLint Unif_translate_y;
+
+
+float translate_x = 0.0;
+float translate_y = 0.0;
+
+
 // Вершина
 struct Vertex
 {
     GLfloat x;
     GLfloat y;
+    GLfloat z;
 };
+
+
 
 // Исходный код вершинного шейдера
 const char* VertexShaderSource = R"(
     #version 330 core
-    in vec2 coord;
+
+    // Координаты вершины. Атрибут, инициализируется через буфер.
+    in vec3 vertexPosition;
+    in vec4 color;
+
+    uniform float translate_y;
+    uniform float translate_x;
+
+    
+    // Выходной параметр с координатами вершины, интерполируется и передётся во фрагментный шейдер 
+    out vec3 vPosition;
+    out vec4 vert_color;
+
     void main() {
-        gl_Position = vec4(coord, 0.0, 1.0);
+        // Передаём непреобразованную координату во фрагментный шейдер
+        vPosition = vertexPosition;
+
+        // Захардкодим углы поворота
+        float x_angle = 185;
+        float y_angle = 0;
+        
+        // Поворачиваем вершину
+        vec3 position = vertexPosition * mat3(
+            1, 0, 0,
+            0, cos(x_angle), -sin(x_angle),
+            0, sin(x_angle), cos(x_angle)
+        ) * mat3(
+            cos(y_angle), 0, sin(y_angle),
+            0, 1, 0,
+            -sin(y_angle), 0, cos(y_angle)
+        );
+
+        position[0] = position[0] + translate_x;
+        position[1] = position[1] + translate_y;
+
+        // Присваиваем вершину волшебной переменной gl_Position
+        gl_Position = vec4(position, 1.0);
+        vert_color = color;
     }
 )";
 
 // Исходный код фрагментного шейдера
 const char* FragShaderSource = R"(
     #version 330 core
+    in vec4 vert_color;
+
     out vec4 color;
     void main() {
-        color = vec4(1, 0, 0, 1);
+        color = vert_color;
     }
 )";
 
@@ -48,7 +102,7 @@ void Release();
 
 
 int main() {
-    sf::Window window(sf::VideoMode(600, 600), "My OpenGL window", sf::Style::Default, sf::ContextSettings(24));
+    sf::Window window(sf::VideoMode(1200, 1200), "My OpenGL window", sf::Style::Default, sf::ContextSettings(24));
     window.setVerticalSyncEnabled(true);
 
     window.setActive(true);
@@ -59,6 +113,7 @@ int main() {
     Init();
 
     while (window.isOpen()) {
+       
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -66,6 +121,27 @@ int main() {
             }
             else if (event.type == sf::Event::Resized) {
                 glViewport(0, 0, event.size.width, event.size.height);
+            }
+        }
+
+        if (event.type == event.KeyPressed) {
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            if (event.key.code == sf::Keyboard::Up)
+            {
+                translate_y += 0.01;
+            }
+            if (event.key.code == sf::Keyboard::Down)
+            {
+                translate_y -= 0.01;
+            }
+            if (event.key.code == sf::Keyboard::Left)
+            {
+                translate_x -= 0.01;
+            }
+            if (event.key.code == sf::Keyboard::Right)
+            {
+                translate_x += 0.01;
             }
         }
 
@@ -114,18 +190,52 @@ void ShaderLog(unsigned int shader)
 
 void InitVBO()
 {
-    glGenBuffers(1, &VBO);
-    Vertex pentagon[] = {
-        { 1.0, 0.0 }, 
-        { 0.309017003, 0.951056540 }, 
-        { -0.809017003, 0.587785244},
-        { -0.809017003, -0.587785244 }, 
-        { 0.309017003, -0.951056540 },
+    glGenBuffers(1, &VBO_position);
+    glGenBuffers(1, &VBO_color);
+    // Вершины тетраэдра
+    Vertex triangle[] = {
+        { 0.0f, 0.0f, 0.16f }, 
+        { 0.0f, 0.3f, 0.0f }, 
+        { 0.3f, -0.3f, 0.0f },
+
+        { 0.0f, 0.0f, 0.16f },
+        { 0.3f, -0.3f, 0.0f },
+        { -0.3f, -0.3f, 0.0f },
+
+        { 0.0f, 0.0f, 0.16f },
+        { -0.3f, -0.3f, 0.0f },
+        { 0.0f, 0.3f, 0.0f },
+
+       { 0.0f, 0.3f, 0.0f },
+       { 0.3f, -0.3f, 0.0f },
+       { -0.3f, -0.3f, 0.0f }, 
     };
-    
+
+    // Цвет треугольника
+    float colors[12][4] = {
+        { 1.0, 1.0, 1.0, 1.0 },
+        { 0.0, 0.0, 1.0, 1.0 },
+        { 0.0, 1.0, 0.0, 1.0 },
+
+        { 1.0, 1.0, 1.0, 1.0 },
+        { 0.0, 1.0, 0.0, 1.0 },
+        { 1.0, 0.0, 0.0, 1.0 },
+
+        { 1.0, 1.0, 1.0, 1.0 },
+        { 1.0, 0.0, 0.0, 1.0 },
+        { 0.0, 0.0, 1.0, 1.0 },
+
+        { 0.0, 0.0, 1.0, 1.0 },
+        { 0.0, 1.0, 0.0, 1.0 },
+        { 1.0, 0.0, 0.0, 1.0 },
+    };
+
+
     // Передаем вершины в буфер
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(pentagon), pentagon, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_position);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
     checkOpenGLerror();
 }
 
@@ -166,11 +276,29 @@ void InitShader() {
     }
 
     // Вытягиваем ID атрибута из собранной программы
-    const char* attr_name = "coord";
+    const char* attr_name = "vertexPosition";
     Attrib_vertex = glGetAttribLocation(Program, attr_name);
     if (Attrib_vertex == -1)
     {
         std::cout << "could not bind attrib " << attr_name << std::endl;
+        return;
+    }
+
+    // Вытягиваем ID юниформ
+    const char* unif_name_x = "translate_x";
+    Unif_translate_x = glGetUniformLocation(Program, unif_name_x);
+    if (Unif_color == -1)
+    {
+        std::cout << "could not bind uniform " << unif_name_x << std::endl;
+        return;
+    }
+
+    // Вытягиваем ID юниформ
+    const char* unif_name_y = "translate_y";
+    Unif_translate_y = glGetUniformLocation(Program, unif_name_y);
+    if (Unif_color == -1)
+    {
+        std::cout << "could not bind uniform " << unif_name_y << std::endl;
         return;
     }
 
@@ -180,25 +308,41 @@ void InitShader() {
 void Init() {
     InitShader();
     InitVBO();
+    // Включаем проверку глубины
+    glEnable(GL_DEPTH_TEST);
 }
 
 
 void Draw() {
     // Устанавливаем шейдерную программу текущей
     glUseProgram(Program);
-    // Включаем массив атрибутов
+
+    // Передаем юниформ в шейдер
+    glUniform1f(Unif_translate_x, translate_x);
+    glUniform1f(Unif_translate_y, translate_y);
+
+    // Включаем массивы атрибутов
     glEnableVertexAttribArray(Attrib_vertex);
-    // Подключаем VBO
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Указывая pointer 0 при подключенном буфере, мы указываем что данные в VBO
-    glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(Attrib_color);
+
+    // Подключаем VBO_position
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_position);
+    glVertexAttribPointer(Attrib_vertex, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    // Подключаем VBO_color
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+    glVertexAttribPointer(Attrib_color, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
     // Отключаем VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     // Передаем данные на видеокарту(рисуем)
-    glDrawArrays(GL_POLYGON, 0, 5);
-    // Отключаем массив атрибутов
+    glDrawArrays(GL_TRIANGLES, 0, 12);
+
+    // Отключаем массивы атрибутов
     glDisableVertexAttribArray(Attrib_vertex);
-    // Отключаем шейдерную программу
+    glDisableVertexAttribArray(Attrib_color);
+
     glUseProgram(0);
     checkOpenGLerror();
 }
@@ -216,7 +360,8 @@ void ReleaseShader() {
 void ReleaseVBO()
 {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &VBO_position);
+    glDeleteBuffers(1, &VBO_color);
 }
 
 void Release() {
